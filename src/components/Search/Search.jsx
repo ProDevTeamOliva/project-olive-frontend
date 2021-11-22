7589e234ac2e79b746614f81d47a71f4c6f4ba15
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-11-22 17:00:53 +0100

    PO-57_search_component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import { Input } from "@chakra-ui/input";
+
+const Search = () => {
+  return (
+    <Input
+      variant="unstyled"
+      placeholder="Szukaj"
+      w={["50%", "80%", "50%", "40%", "30%"]}
+      bgColor="rgba(255, 255, 255, 0.2)"
+      border="none"
+      borderRadius="10px"
+      p="0 30px"
+      lineHeight="40px"
+      _placeholder={{ color: "white" }}
+      display={["none", "block"]}
+    />
+  );
+};
+
+export default Search;
