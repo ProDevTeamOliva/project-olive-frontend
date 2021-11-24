376551d97d057e1251f271eb548a05a48fe262d3
Wiktor Morawski <wiktormorawski@MacBook-Air-Wiktor.local>
2021-11-24 13:20:10 +0100

    PO-59_added_languages_to_the_app_pl_en

--- src/App.js
+++ src/App.js
@@ -10,8 +10,42 @@ import LogInPage from "./components/LogInPage/LogInPage";
 import MainPage from "./components/MainPage/MainPage";
 import NoPermission from "./components/MainPage/NoPermission";
 import { connect } from "react-redux";
+import i18next from 'i18next';
+import { initReactI18next, useTranslation } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import languages from './config/languages';
+
+const language = languages.find(value => value === localStorage.getItem('language'));
+
+i18next.use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: language || 'en',
+    fallbackLng: 'en',
+    ns: [ 'main' ],
+    defaultNS: 'main',
+    react: {
+      wait: true,
+      useSuspense: false
+    },
+    interpolation: {
+      escapeValue: false
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    }
+  })
+
 
 function App({ auth, logIn, signUp }) {
+  
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem('language', lng);
+  }
+  
   const isAuth = auth.isAuth;
 
   const loginSubmit = (data) => {
@@ -20,6 +54,8 @@ function App({ auth, logIn, signUp }) {
 
   return (
     <div className="App" style={{ height: "100%" }}>
+      <button onClick={() => changeLanguage('pl')}>PL</button>
+      <button onClick={() => changeLanguage('en')}>EN</button>
       <Router>
         <Switch>
           <Route exact path="/">
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,11 @@
 import { Input } from "@chakra-ui/input";
-
+import {useTranslation} from 'react-i18next'
 const Search = () => {
+  const { t } = useTranslation()
   return (
     <Input
       variant="unstyled"
-      placeholder="Szukaj"
+      placeholder={t('SearchNavBar')}
       w={["50%", "80%", "50%", "40%", "30%"]}
       bgColor="rgba(255, 255, 255, 0.2)"
       border="none"
