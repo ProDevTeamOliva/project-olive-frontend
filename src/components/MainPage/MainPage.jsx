c7a495b6a7f2118b44d2d93821bb862601d55552
Wiktor Morawski <wiktormorawski@MacBook-Air-Wiktor.local>
2022-01-23 17:20:44 +0100

    PO-73_utworzenie_modulara_do_dodawania_komentarza_do_posta

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,8 +6,8 @@ function MainPage() {
     <Box h="100vh" mt="75px" d="grid" justifyContent="center">
       <Navbar />
       <Grid m="25px" gap={5}>
-      {/* <Grid templateColumns="minmax(200px, 500px)" gap={5} m="25px"> */}
-        <Post/>
+        {/* <Grid templateColumns="minmax(200px, 500px)" gap={5} m="25px"> */}
+        <Post />
       </Grid>
     </Box>
   );
