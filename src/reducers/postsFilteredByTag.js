254087390bf376548abd1d29d1c6ddee1a94ec0e
Maciej Tokarz <maciej.tokarz1@wp.pl>
2022-05-10 22:12:40 +0200

    PO-0_fixed_cleaning_search_results

--- src/reducers/postsFilteredByTag.js
+++ src/reducers/postsFilteredByTag.js
@@ -2,7 +2,7 @@ import { TAGS_SUCCESS, TAGS_FAILURE } from "../types/postTypes";
 
 const init_state = [];
 
-const postsFiltered = (state = init_state, action) => {
+const postsFilteredByTag = (state = init_state, action) => {
   switch (action.type) {
     case TAGS_SUCCESS:
       return action.payload.posts;
@@ -15,4 +15,4 @@ const postsFiltered = (state = init_state, action) => {
   }
 };
 
-export default postsFiltered;
+export default postsFilteredByTag;
