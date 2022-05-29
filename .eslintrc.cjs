2fc305b444aa0c82b4e40717ae66fce768463335
Maciej Tokarz <maciej.tokarz1@wp.pl>
2022-05-29 12:00:13 +0200

    PO-0_fixed_responsiveness

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    plugins: ["react"],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        "react/prop-types": "off",
+        "react/display-name": "off",
+    },
+};
--- src/components/InfinityScroll/LoadMore.jsx
+++ src/components/InfinityScroll/LoadMore.jsx
@@ -4,27 +4,31 @@ import { memo } from "react";
 import LoadingSpinner from "../Spinner/LoadingSpinner";
 
 const LoadMore = ({
-  isMore,
-  isLoading,
-  onClickLoadMore,
-  loadMoreText,
-  noMoreText,
+    isMore,
+    isLoading,
+    onClickLoadMore,
+    loadMoreText,
+    noMoreText,
 }) => {
-  if (isMore && !isLoading) {
-    return <Button onClick={onClickLoadMore}>{loadMoreText}</Button>;
-  }
-  if (isLoading) {
-    return <LoadingSpinner isLoading={isLoading} />;
-  }
-  if (!isMore) {
-    return (
-      <Center>
-        <InfoIcon />
-        <Text padding={"3"}>{noMoreText}</Text>
-      </Center>
-    );
-  }
-  return null;
+    if (isMore && !isLoading) {
+        return (
+            <Button mb="20px" onClick={onClickLoadMore}>
+                {loadMoreText}
+            </Button>
+        );
+    }
+    if (isLoading) {
+        return <LoadingSpinner isLoading={isLoading} />;
+    }
+    if (!isMore) {
+        return (
+            <Center mb="20px">
+                <InfoIcon />
+                <Text px={"3"}>{noMoreText}</Text>
+            </Center>
+        );
+    }
+    return null;
 };
 
 export default memo(LoadMore);
